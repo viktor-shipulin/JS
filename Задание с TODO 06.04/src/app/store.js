b0e41e3faa3d0d2c9e269e14431cdb324aca5f53
viktor-shipulin <130042307+viktor-shipulin@users.noreply.github.com>
2026-04-06 16:58:24 +0600

    rename to todo-app

--- Задание с TODO 06.04/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todosReducer from '../features/todos/todosSlice';
-
-export const store = configureStore({
-    reducer: {
-        todos: todosReducer,
-    }
-});
